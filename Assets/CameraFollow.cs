54723597c63fbce985e8c35cfdeecba038287f5b
huffman442 <huffman442@yahoo.com>
2023-02-04 11:06:29 -0500

    add camera following

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform target;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetPosition = offset;
+        targetPosition.y = target.position.y + offset.y;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
